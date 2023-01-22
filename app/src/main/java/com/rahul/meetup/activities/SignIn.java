797719fe9e97c0b82044c04aeef11722cc5032f9
Rahul Rasve <rahulrasve712@gmail.com>
2023-01-22 07:40:49 +0530

    Login Activity Completed

--- app/src/main/java/com/rahul/meetup/activities/SignIn.java
+++ app/src/main/java/com/rahul/meetup/activities/SignIn.java
@@ -5,32 +5,107 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.meetup.R;
 import com.example.meetup.databinding.ActivitySignInBinding;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.rahul.meetup.utilities.Constants;
+import com.rahul.meetup.utilities.PreferenceManager;
 
+import java.lang.ref.PhantomReference;
 import java.util.HashMap;
+import java.util.regex.Pattern;
 
 public class SignIn extends AppCompatActivity {
 
     private ActivitySignInBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySignInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
         setListeners();
     }
 
     private void setListeners(){
         binding.registerText.setOnClickListener(v ->
                 startActivity(new Intent(getApplicationContext(), Register.class)));
+        binding.loginButton.setOnClickListener(v -> {
+            if(isValidDetails()){
+                singIn();
+            }
+        });
     }
 
-    private void addDataToFirestore(){
-        FirebaseFirestore databse = FirebaseFirestore.getInstance();
+    private void singIn(){
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .whereEqualTo(Constants.KEY_EMAIL, binding.inputEmail.getText().toString())
+                .whereEqualTo(Constants.KEY_PASSWORD, binding.inputPassword.getText().toString())
+                .get()
+                .addOnCompleteListener(task -> {
+                    if(task.isSuccessful() && task.getResult() != null
+                       && task.getResult().getDocuments().size()>0){
+                        DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+                        preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
+                        preferenceManager.putString(Constants.KEY_USER_ID, documentSnapshot.getId());
+                        preferenceManager.putString(Constants.KEY_NAME, documentSnapshot.getString(Constants.KEY_NAME));
+                        preferenceManager.putString(Constants.KEY_IMAGE, documentSnapshot.getString(Constants.KEY_IMAGE));
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    }
+                    else{
+                        loading(false);
+                        createToast("Failed to Login");
+                    }
+                });
+    }
+
+    private void loading(Boolean isLoading){
+        if(isLoading){
+            binding.loginButton.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        }
+        else{
+            binding.loginButton.setVisibility(View.VISIBLE);
+            binding.progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void addDataToFireStore(){
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
         HashMap<String, Object> data = new HashMap<>();
     }
+
+    private void createToast(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private Boolean isValidDetails(){
+        if(binding.inputEmail.getText().toString().trim().isEmpty()){
+            createToast("Enter Email");
+            return false;
+        }
+        else if(!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()){
+            createToast("Enter Valid Email Address");
+            return false;
+        }
+        else if(binding.inputPassword.getText().toString().trim().isEmpty()){
+            createToast("Enter Password");
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
 }
